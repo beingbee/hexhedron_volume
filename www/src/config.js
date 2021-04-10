692fc422841dc7deb1175b54b186c5c8b2777659
Dongho Chang <thebeingbee@gmail.com>
2021-04-10 21:08:20 +0000

    Frontend deployment bug fixed caused by git branch name mismatch
    - git default branch name is modified from master to main

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "4rjvs932p9o05g4os5cnft3gh8",     // CognitoClientID
+  "aws_user_pools_web_client_id": "10ce4qt5vlq3oaduor6s9nu6na",     // CognitoClientID
   "api_base_url": "",                                     // TodoFunctionApi
   "cognito_hosted_domain": "hexvolume-hexvol.auth.ap-northeast-2.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://master.d2enz6aqt9ena3.amplifyapp.com"                                      // AmplifyURL
+  "redirect_url": "https://main.d1ri7z75rgvseu.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
