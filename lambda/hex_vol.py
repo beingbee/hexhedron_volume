329684958a3e41a2bbefcd9171db3e358d1a887c
Dongho Chang <thebeingbee@gmail.com>
2021-04-11 06:28:15 +0000

    Implement backend lambda function
    - TODO: To examine exact request body data, the request data is just
      logged and DO NOT used in real volume calculation
    - After request data is exactly aquired, this implementation must
      be modified

--- lambda/hex_vol.py
+++ lambda/hex_vol.py
@@ -1,21 +1,34 @@
 import json
 import numpy as np
 from scipy.spatial import ConvexHull
+import logging
 
 
-def calculate_volume():
+def calculate_volume(vertex):
     points = np.array(input)
     hull = ConvexHull(points)
     return hull.volume
 
+
+def process_input(event):
+    # TODO. temporary return only fixed input
+
+    logging.info(event)
+    input = [ [0, 0, 0], [ 0, 1, 0], [1, 0, 0], [1, 1, 0], [0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 2]]
+    # input validation. return exception.
+    # convert input to vertex list
+    return input
+
+
 def lambda_handler(event, context):
-    print(event)
+    input = process_input(event)
+    volume = calculate_volume(input)
     return {
         "statusCode": 200,
         'headers': {
             "Access-Control-Allow-Origin": "*"
         },
         "body": json.dumps({
-            "message": "hello world",
+            "volume": volume,
         }),
     }
