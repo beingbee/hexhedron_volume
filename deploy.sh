ebe91110308c9ac89e96b65ceee8e21784ab32e4
Dongho Chang <thebeingbee@gmail.com>
2021-04-11 08:58:59 +0000

    typos in deployment script fixed

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,9 @@ commit="no"
 sam build  || { echo "Error!! build failure"; exit 1; }
 sam deploy --guided --stack-name $STACK_NAME  || { echo "Error!! deploy failure"; exit 1; }
 
-
 export AWS_COGNITO_REGION=$AWS_DEFAULT_REGION
 export AWS_USER_POOLS_WEB_CLIENT_ID=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='CognitoClientID'].OutputValue" --output text`
-export API_BASE_URL=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='TodoFunctionApi'].OutputValue" --output text`
+export API_BASE_URL=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='HexVolFunctionApi'].OutputValue" --output text`
 export STAGE_NAME_PARAM=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Parameters[?ParameterKey=='StageNameParam'].ParameterValue" --output text`
 export COGNITO_HOSTED_DOMAIN=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='CognitoDomainName'].OutputValue" --output text`
 export REDIRECT_URL=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='AmplifyURL'].OutputValue" --output text`
