68e6273ef569c0f115ed7e36de0af8ea26bb0b0f
Dongho Chang <thebeingbee@gmail.com>
2021-04-11 12:49:21 +0000

    frontend layout enchanced

--- www/src/HexVol.js
+++ www/src/HexVol.js
@@ -7,23 +7,19 @@ function HexVol({ hexVolume, calcHexVol }) {
 
   return (
     <div className="HexVol">
-      <Row>
-        <Col xs="12" className="mt-1 mb-1">
-          <Form inline>
-            <FormGroup>
-              <Label for="newVertices">Input 2x4 Vertices List e.g.[[0,0,0],[1,0,0],[0,1,0],[1,1,0]], [[0,0,1],[1,0,1],[0,1,1],[1,1,1]] </Label>
-              <Input type="text" name="newVertices" id="newVertices" placeholder="2 x 4 vertices list" />
-            </FormGroup>
-            <Button onClick={calcHexVol} color="primary" className="ml-1">Calculate</Button>
-          </Form>
-        </Col>
-      </Row>
+      <Form inline>
+        <FormGroup>
+          <Label for="newVertices">Input 2x4 Vertices List (e.g.[[0,0,0],[1,0,0],[0,1,0],[1,1,0]], [[0,0,1],[1,0,1],[0,1,1],[1,1,1]]) </Label>
+          <Input type="text" name="newVertices" id="newVertices" placeholder="2 x 4 vertices list" />
+        </FormGroup>
+        <Button onClick={calcHexVol} color="primary" className="ml-1">Calculate</Button>
+      </Form>
       <Row>
         <Col xs="12" className="mt-1 mb-1">
            Volume is {hexVolume}
         </Col>
       </Row>
-    </div >
+    </div>
   );
 }
 
