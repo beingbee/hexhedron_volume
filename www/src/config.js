010dc0fdefec15e0052996edcd22c56dafeb7ee4
Dongho Chang <thebeingbee@gmail.com>
2021-04-11 08:40:54 +0000

    Config.js typo fixed
    - API GW base url is fixed

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "4selhoe9k14de4j5vosc3e3jbl",     // CognitoClientID
+  "aws_user_pools_web_client_id": "15rt7j0cltglrq4u27lp4mf6qm",     // CognitoClientID
   "api_base_url": "",                                     // TodoFunctionApi
   "cognito_hosted_domain": "hexvolume-hexvol.auth.ap-northeast-2.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://main.d1tmumvptz3g4y.amplifyapp.com"                                      // AmplifyURL
+  "redirect_url": "https://main.d31f0llbvkl454.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
